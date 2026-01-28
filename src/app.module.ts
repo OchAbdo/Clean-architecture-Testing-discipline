74f4d2981fbc0c5da76d04b9e8250f8d6d1e7929
OchAbdo <ochabdo02@gmail.com>
2026-01-28 11:25:56 +0100

    database connection assignment and use .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USER"),
+        password: configService.get<string>("DB_PASS"),
+        database: configService.get<string>("DB_NAME"),
+        autoLoadEntities: true,
+        synchronize: true,
+      })
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
